0d1935cb243686b2fe5e2632da53e468a2d50a6b
omondimaurine <omondimaurine713@gmail.com>
2024-03-26 12:05:06 +0300

    array-find method in javascript

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(denverWinning){
+    const result = denverWinning.find(denverWinning =>denverWinning.year=="2015")
+    return !!result? result.year:undefined;
+
+    
+}
+superbowlWin;
+
